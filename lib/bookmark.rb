56366985211e75769a3f67198114d128927c4987
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-11-04 10:36:37 +0000

    bookmark class refactored

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,31 +2,28 @@ require 'pg'
 class Bookmark
 
   def self.all
-    if ENV['RACK_ENV'] == 'test'
-      conn = PG.connect(:dbname => 'bookmark_manager_test') # connecting to database
-      table  = conn.exec('SELECT * FROM bookmarks;') # within database, connecting to table
-    else
-      conn = PG.connect(:dbname => 'bookmark_manager') # connecting to database
-      table  = conn.exec('SELECT * FROM bookmarks;') # within database, connecting to table
-    end
+
+    table  = connetion_choice.exec('SELECT * FROM bookmarks;') # within database, connecting to table
     table.map { |result| [result['url'], result['title'], result['id']]}
   end
 
   def self.add(url, title)
-    if ENV['RACK_ENV'] == 'test'
-      conn = PG.connect(:dbname => 'bookmark_manager_test') # connecting to database
-    else
-      conn = PG.connect(:dbname => 'bookmark_manager') # connecting to database
-    end
-    conn.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}')")
+    connetion_choice.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}')")
   end
 
   def self.remove(id)
+    connetion_choice.exec("DELETE FROM bookmarks WHERE id='#{id}'")
+  end
+
+  def self.updatde(id)
+  end
+
+  private
+  def self.connetion_choice
     if ENV['RACK_ENV'] == 'test'
-      conn = PG.connect(:dbname => 'bookmark_manager_test') # connecting to database
+      PG.connect(:dbname => 'bookmark_manager_test') # connecting to database
     else
-      conn = PG.connect(:dbname => 'bookmark_manager') # connecting to database
+      PG.connect(:dbname => 'bookmark_manager') # connecting to database
     end
-    conn.exec("DELETE FROM bookmarks WHERE id='#{id}'")
   end
 end
